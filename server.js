02a2d1c450c9282eeccd1946e5c0d89321e5b166
Louis Mbuyu <louismbuyu17@gmail.com>
2016-08-30 03:28:03 +0200

    Add login route and authenticate user

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -120,6 +121,7 @@ app.put('/todos/:id', function(req, res){
 	});
 });
 
+//CREATE new user
 app.post('/users', function(req,res){
 	var body = _.pick(req.body, 'email', 'password');
 
@@ -130,7 +132,18 @@ app.post('/users', function(req,res){
 	})
 })
 
-db.sequelize.sync().then(function(){
+//POST user/login
+app.post('/users/login', function(req,res){
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user){
+		res.json(user.toPublicJSON());
+	}, function(){
+		res.status(401).send();
+	});
+});
+
+db.sequelize.sync({force:true}).then(function(){
 
 	app.listen(PORT, function(){
 		console.log('Express ist f listening on port '+ PORT +'!');
